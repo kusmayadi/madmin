2e9c1a0225b034a2e9e36e2cc07975e5fcce3c3c
Kusmayadi <kusmayadi@cobacobi.com>
2012-10-27 23:23:27 +0700

    Fixed Cobacobi_Controller_Login name issue

--- classes/cobacobi/controller/login.php
+++ classes/cobacobi/controller/login.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Cobacobi_Login extends Controller_Admin {
+class Cobacobi_Controller_Login extends Controller_Admin {
 
 	public function before()
 	{
