4cb6418a59c46e1f9b7845a3e7da62b4ee490dcc
Kusmayadi <kusmayadi@cobacobi.com>
2013-01-24 12:21:36 +0700

    Adding default css

--- classes/cobacobi/controller/admin.php
+++ classes/cobacobi/controller/admin.php
@@ -37,6 +37,7 @@ class Cobacobi_Controller_Admin extends Cobacobi_Controller_Template {
 		{
 			$this->add_css('bootstrap.min.css');
 			$this->add_css('bootstrap-responsive.min.css');
+			$this->add_css('admin.css');
 		
 			// Add global js to template
 			$this->add_js('jquery-1.9.0.min.js');
