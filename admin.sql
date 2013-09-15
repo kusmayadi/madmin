be50bed1d5473517c8576de77f8da24eaca8a826
Kusmayadi <kusmayadi@cobacobi.com>
2013-09-15 17:58:34 +0700

    Add  field to role

--- admin.sql
+++ admin.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `users` ADD COLUMN `name` VARCHAR(75) NULL DEFAULT NULL  AFTER `id` ;
 
 UPDATE `roles` SET `description` = 'Login privileges, grant user to login to the system' WHERE `id` = 1;
+
+ALTER TABLE `roles` ADD COLUMN `removable` SMALLINT NOT NULL DEFAULT 1;
+
+UPDATE `roles` SET `removable` = 0 WHERE `id` IN (1,2);
