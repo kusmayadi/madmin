1f31f52e8b4dc25f9a68964fcf0bb7785ab360ad
Kusmayadi <kusmayadi@cobacobi.com>
2013-09-15 18:29:21 +0700

    Fix layout on update profile

--- views/default/profile/profile.php
+++ views/default/profile/profile.php
@@ -11,33 +11,32 @@ echo Form::open(NULL, array('class' => 'form-horizontal'));
 
 	// Name
 	echo '<div class="control-group">';
-	echo Form::label('name', __('Name'), array('class' => 'control-label'));
-	echo '<div class="controls">';
-		echo Form::input('name', $name);
-	echo '</div>';
-	if (isset($errors['name']))
-		echo '<div class="error">'.$errors['name'].'</div>';
+		echo Form::label('name', __('Name'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::input('name', $name);
+			if (isset($errors['name']))
+				echo ' <small class="text-error">'.ucfirst($errors['name']).'</small>';
+		echo '</div>';
 	echo '</div>';
 			
 	// Email
 	echo '<div class="control-group">';
-	echo Form::label('email', __('Email'), array('class' => 'control-label'));
-	echo '<div class="controls">';
-		echo Form::input('email', $email);
-	echo '</div>';
-	if (isset($errors['email']))
-		echo '<div class="error">'.$errors['email'].'</div>';
+		echo Form::label('email', __('Email'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::input('email', $email);
+			if (isset($errors['email']))
+				echo ' <small class="text-error">'.ucfirst($errors['email']).'</small>';
+		echo '</div>';
 	echo '</div>';
 		
 	// Password
 	echo '<div class="control-group">';
-	echo Form::label('password', __('Enter your password to update your profile'), array('class' => 'control-label'));
-	echo '<div class="controls">';
-		echo Form::password('password');
-		if (isset($errors['password']))
-			echo ' <small class="text-error">'.ucfirst($errors['password']).'</small>';
-	echo '</div>';
-	
+		echo Form::label('password', __('Enter your password to update your profile'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::password('password');
+				if (isset($errors['password']))
+					echo ' <small class="text-error">'.ucfirst($errors['password']).'</small>';
+		echo '</div>';
 	echo '</div>';
 	
 	echo '<div class="controls">';
