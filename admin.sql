729849c7c367a5f081ac37bc087fbd90d0a6d688
Kusmayadi <kusmayadi@cobacobi.com>
2013-09-15 17:23:13 +0700

    Change login role description

--- admin.sql
+++ admin.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `users` ADD COLUMN `name` VARCHAR(75) NULL DEFAULT NULL  AFTER `id` ;
+
+UPDATE `roles` SET `description` = 'Login privileges, grant user to login to the system' WHERE `id` = 1;
