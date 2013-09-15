10f89c78cdd8f28ffed4c6d83f2339acf533ce7e
Kusmayadi <kusmayadi@cobacobi.com>
2013-09-15 18:29:01 +0700

    Fix layout on change password

--- views/default/profile/changepassword.php
+++ views/default/profile/changepassword.php
@@ -1,36 +1,41 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-		
-echo '<div class="module_content">';
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
 
-	echo form::open();
+<div class="container-fluid"><h1><?php echo __('Change Password'); ?></h1></div>
+		
+<?php echo Form::open(NULL, array('class' => 'form-horizontal'));
 
 	// Old Password
-	echo '<fieldset>';
-	echo form::label('old_password', __('Old Password'));
-	echo form::password('old_password');
-	if (isset($errors['old_password']))
-		echo '<div class="error">'.$errors['old_password'].'</div>';
-	echo '</fieldset>';
+	echo '<div class="control-group">';
+		echo Form::label('old_password', __('Old Password'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::password('old_password');
+			if (isset($errors['old_password']))
+				echo ' <small class="text-error">'.ucfirst($errors['old_password']).'</small>';
+		echo '</div>';
+	echo '</div>';
 			
 	// New Password
-	echo '<fieldset>';
-	echo form::label('password', __('New Password'));
-	echo form::password('password');
-	if (isset($errors['password']))
-		echo '<div class="error">'.$errors['password'].'</div>';
-	echo '</fieldset>';
+	echo '<div class="control-group">';
+		echo Form::label('password', __('New Password'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::password('password');
+			if (isset($errors['password']))
+				echo ' <small class="text-error">'.ucfirst($errors['password']).'</small>';
+		echo '</div>';
+	echo '</div>';
 		
 	// Password Confirm
-	echo '<fieldset>';
-	echo form::label('password_confirm', __('Re-type new password'));
-	echo form::password('password_confirm');
-	if (isset($errors['password_confirm']))
-		echo '<div class="error">'.$errors['password_confirm'].'</div>';
-	echo '</fieldset>';
+	echo '<div class="control-group">';
+		echo Form::label('password_confirm', __('Re-type new password'), array('class' => 'control-label'));
+		echo '<div class="controls">';
+			echo Form::password('password_confirm');
+				if (isset($errors['password_confirm']))
+					echo ' <small class="text-error">'.ucfirst($errors['password_confirm']).'</small>';
+		echo '</div>';
+	echo '</div>';
 		
-			
-	echo form::submit(NULL, ucfirst(strtolower($module_action_title)));
+	echo '<div class="controls">';
+		echo Form::submit(NULL, ucfirst(strtolower($module_action_title)), array('class' => 'btn btn-primary'));
+	echo '</div>';
 	
-	echo form::close();
-
-echo '</div>';
+echo Form::close();
