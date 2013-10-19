eb6891994e174527d6979f1cbc14595136aff605
Kusmayadi <kusmayadi@cobacobi.com>
2013-10-19 13:50:31 +0700

    Change default role for user generator to admin

--- classes/Task/User/Generate.php
+++ classes/Task/User/Generate.php
@@ -9,8 +9,8 @@
  * - email: User's email. Default: admin@localhost
  * - role: User's role. Default: user
  * 
- * @author		Kusmayadi (kusmayadi@cobacobi.com)
- * @copyright	(c) 2013 Kusmayadi
+ * @author    Kusmayadi (kusmayadi@cobacobi.com)
+ * @copyright (c) 2013 Kusmayadi
  */
 class Task_User_Generate extends Minion_Task
 {
@@ -19,25 +19,25 @@ class Task_User_Generate extends Minion_Task
         'password' => '4dm1n',
         'name' => 'Administrator',
         'email' => 'admin@localhost.com',
-        'role' => 'user'
+        'role' => 'admin'
     );
  
     protected function _execute(array $params)
     {
         if ( ! isset($params['username']))
-        	$params['username'] = 'admin';
-        	
+          $params['username'] = 'admin';
+          
         if ( ! isset($params['password']))
-        	$params['password'] = '4dm1n';
-        	
+          $params['password'] = '4dm1n';
+          
         if (! isset($params['name']))
-        	$params['name'] = 'Administrator';
-        	
+          $params['name'] = 'Administrator';
+          
         if ( ! isset($params['email']))
-        	$params['email'] = 'admin@localhost.com';
-        	
+          $params['email'] = 'admin@localhost.com';
+          
         if ( !isset($params['role']))
-        	$params['role'] = 'user';
+          $params['role'] = 'admin';
         
         $user = ORM::factory('user');
         $user->username = $params['username'];
@@ -54,8 +54,8 @@ class Task_User_Generate extends Minion_Task
         // Add aditional role
         if ($params['role'] != 'user' AND $params['role'] != 'login')
         {
-        	$role = ORM::factory('role')->where('name', '=', $params['role'])->find();
-        	$user->add('roles', $role);
+          $role = ORM::factory('role')->where('name', '=', $params['role'])->find();
+          $user->add('roles', $role);
         }
         
         echo $params['username'] . ' is sucessfully created';
