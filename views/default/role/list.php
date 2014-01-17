105f5db0bbe1c7c42c85e5bb4a54978c8aa7602e
Kusmayadi <kusmayadi@gmail.com>
2014-01-18 01:34:48 +0800

    Add disabling check option for unremovable roles

--- views/default/role/list.php
+++ views/default/role/list.php
@@ -1,28 +1,28 @@
 <h1><?php echo __('Roles'); ?></h1>
 
-<table class="table table-bordered table-hover"> 
-	<thead> 
-		<tr> 
-   			<th class="header" width="1"></th> 
-    		<th class="header"><?php echo __('Role'); ?></th>
-    		<th class="header"><?php echo __('Description'); ?></th>
-			<th class="header"><?php echo __('Actions'); ?></th> 
-		</tr> 
-	</thead> 
-		
-	<tbody> 
-		<?php foreach($pagination->result() as $role): ?>
-		<tr> 
-			<td><input type="checkbox" class="chkId" value="<?php echo $role->id; ?>"></td> 
-			<td><?php echo html::anchor('role/update/'.$role->id, ucwords($role->name)); ?></td> 
-			<td><?php echo $role->description; ?></td> 
-			<td width="140">
-				<?php echo html::anchor('role/update/'.$role->id, '<i class="icon-edit"></i> '.__('Edit'), array('class' => 'btn btn-mini')); echo ' &nbsp; '; ?>
-				<?php echo ($role->removable) ? html::anchor('role/delete?ids='.$role->id, '<i class="icon-trash"></i> '.__('Remove'), array('class' => 'btn btn-mini delete_link')) : ''; ?>
-			</td> 
-		</tr> 
-		<?php endforeach; ?>
-	</tbody> 
+<table class="table table-bordered table-hover">
+  <thead>
+    <tr>
+        <th class="header" width="1"></th>
+        <th class="header"><?php echo __('Role'); ?></th>
+        <th class="header"><?php echo __('Description'); ?></th>
+      <th class="header"><?php echo __('Actions'); ?></th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php foreach($pagination->result() as $role): ?>
+    <tr>
+      <td><input type="checkbox" class="chkId"<?php echo ($role->removable)? '' : 'disabled="disabled"'; ?> value="<?php echo $role->id; ?>"></td>
+      <td><?php echo html::anchor('role/update/'.$role->id, ucwords($role->name)); ?></td>
+      <td><?php echo $role->description; ?></td>
+      <td width="140">
+        <?php echo html::anchor('role/update/'.$role->id, '<i class="icon-edit"></i> '.__('Edit'), array('class' => 'btn btn-mini')); echo ' &nbsp; '; ?>
+        <?php echo ($role->removable) ? html::anchor('role/delete?ids='.$role->id, '<i class="icon-trash"></i> '.__('Remove'), array('class' => 'btn btn-mini delete_link')) : ''; ?>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
 </table>
 
 <button id="delete_btn" class="btn btn-danger" rel="<?php echo url::site('role/delete'); ?>"><?php echo __('Delete selected roles'); ?></button>
